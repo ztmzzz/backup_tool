b547ecd3d1f178bff526d99a2792451f2ee67efb
ztmzzz <ztmzzz@163.com>
2022-10-08 17:51:12 +0800

    界面优化

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -42,6 +42,10 @@ void MainWindow::on_checkBox_stateChanged(int arg1) {
 
 
 void MainWindow::on_backup_clicked() {
+    if (ui->dst_path->text().isEmpty() || ui->src_path->text().isEmpty()) {
+        QMessageBox::warning(this, "警告", "请填写完整信息");
+        return;
+    }
     root_path = ui->src_path->text().toStdString();
     QDir dir(ui->dst_path->text());
     backup_file(ui->src_path->text().toStdString(), ui->dst_path->text().toStdString() + "/backup.tmp");
@@ -50,17 +54,30 @@ void MainWindow::on_backup_clicked() {
     c.compress_file(ui->dst_path->text().toStdString() + "/backup.tmp", ui->dst_path->text().toStdString() + "/backup");
     dir.remove("backup.tmp");
     if (is_encrypt) {
+        if (ui->password->toPlainText().isEmpty()) {
+            QMessageBox::warning(this, "警告", "请填写密码");
+            return;
+        }
         encrypt e(ui->password->toPlainText().toStdString());
         e.encrypt_file(ui->dst_path->text().toStdString() + "/backup",
                        ui->dst_path->text().toStdString() + "/backup.enc");
         dir.remove("backup");
     }
+    QMessageBox::information(this, "备份完成", "备份完成");
 
 }
 
 void MainWindow::on_restore_clicked() {
+    if (ui->dst_path->text().isEmpty() || ui->src_path->text().isEmpty()) {
+        QMessageBox::warning(this, "警告", "请填写完整信息");
+        return;
+    }
     QDir dir(ui->dst_path->text());
     if (is_encrypt) {
+        if (ui->password->toPlainText().isEmpty()) {
+            QMessageBox::warning(this, "警告", "请填写密码");
+            return;
+        }
         encrypt e(ui->password->toPlainText().toStdString());
         e.encrypt_file(ui->dst_path->text().toStdString() + "/backup.enc",
                        ui->dst_path->text().toStdString() + "/backup");
@@ -72,5 +89,6 @@ void MainWindow::on_restore_clicked() {
 //    dir.remove("backup");
     restore_file(ui->dst_path->text().toStdString() + "/backup.tmp", ui->src_path->text().toStdString());
     dir.remove("backup.tmp");
+    QMessageBox::information(this, "恢复完成", "恢复完成");
 }
 
